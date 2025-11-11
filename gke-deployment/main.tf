3c94098c004b4cdab4b045110b1f8b21fb1fe9e5
hruthika2003 <hruthika.sa@cloudambassadors.com>
2025-11-11 17:35:45 +0530

    Update main.tf

--- gke-deployment/main.tf
+++ gke-deployment/main.tf
@@ -63,6 +63,12 @@ resource "google_container_node_pool" "primary_nodes" {
   }
 }
 
+resource "null_resource" "run_script" {
+  provisioner "local-exec" {
+    command = "bash ./setup.sh"
+  }
+}
+
 # Get Cluster Credentials
 data "google_client_config" "default" {}
 
